ee060ece467a7f776dc6ca7c8bbc4fe788c8c1bf
Ben Wendtner <ben.wendtner@gmail.com>
2012-07-03 22:00:18 +0200

    Created a new Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KTong::Application.config.secret_token = '6abcfec7c9b1431e046dd74e0adee4099a024379e8d70f9531e423e8c958ba4b8e82f1d3547124ff0d158eceb3db0100e592e5432e977aef82e59699fd3f99cc'
