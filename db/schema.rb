2a58834723c74d8397dca6534b0a4218ef6cbeb7
Sebastian Poell <sebastian.poell@gmail.com>
2012-07-09 20:13:07 +0200

    changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120709135722) do
+ActiveRecord::Schema.define(:version => 20120709144853) do
 
   create_table "cart_items", :force => true do |t|
-    t.integer "user_id"
-    t.integer "product_id"
-    t.integer "amount",     :null => false
+    t.integer  "user_id"
+    t.integer  "product_id"
+    t.integer  "amount",     :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "products", :force => true do |t|
