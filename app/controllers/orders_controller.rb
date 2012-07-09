de3962d90283b710d3c4d4693802fd7124888c89
Sebastian Poell <sebastian.poell@gmail.com>
2012-07-09 20:44:54 +0200

    some more basic stuff

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,15 @@
 class OrdersController < ApplicationController
   
   def index
+    #@categories = 
+    @product = Product.find(params[:product_id])
+    @cart_item = CartItem.new
     @cart_items = User.find(1).cart_items
+    
+    respond_to do |format|
+      format.html
+      format.json
+    end
   end
   
   
