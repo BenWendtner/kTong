47f43d5f30f8de8ebf59972344443ae816d7a041
Ben Wendtner <ben.wendtner@gmail.com>
2012-07-13 20:49:56 +0200

    Categories

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,19 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120712162410) do
+ActiveRecord::Schema.define(:version => 20120713182929) do
 
   create_table "cart_items", :force => true do |t|
     t.integer  "user_id"
     t.integer  "product_id"
     t.integer  "amount",     :null => false
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "categories", :force => true do |t|
+    t.string   "name"
+    t.integer  "parent_id"
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
