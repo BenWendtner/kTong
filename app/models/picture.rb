2313273aeb555be424821db4d899e99cd5b7204d
Sebastian Poell <sebastian.poell@gmail.com>
2012-07-21 07:53:55 +0200

    cleanup the image-fields and stuff

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -2,6 +2,9 @@ class Picture < ActiveRecord::Base
   
    belongs_to :product
    
+   validates_attachment_size :image, :less_than => 20.megabyte
+   validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] 
+  
    has_attached_file :image, :styles => { 
       :original => ["3000x3000>", :jpeg], 
       :medium => ["276x173>", :jpeg], 
--- app/models/product.rb
+++ app/models/product.rb
@@ -4,14 +4,5 @@ class Product < ActiveRecord::Base
  has_many       :pictures, :order => 'position ASC'
  has_many       :elements
  belongs_to     :category
- 
- validates_attachment_size :image, :less_than => 20.megabyte
- validates_attachment_content_type :image, :content_type => ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
-  
- has_attached_file :image, :styles => { 
-      :original => ["3000x3000>", :jpeg], 
-      :medium => ["276x173#", :jpeg], 
-      :thumb => ["166x105#", :jpeg]
- }
-  
+
 end
