60fb45680bebfd8c396728b2a8c22e7d6c77b6b7
Ben Wendtner <ben.wendtner@gmail.com>
2012-07-23 19:59:52 +0200

    addresses

--- db/migrate/20120723141830_create_orders.rb
+++ db/migrate/20120723141830_create_orders.rb
@@ -1,8 +1,15 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
+        t.integer :user_id, :null => false
         t.date :date, :null => false
 
+        t.string firstname, :null => false
+        t.string lastname, :null => false
+        t.string address, :null => false
+        t.string location, :null => false
+        t.integer postalcode, :null => false
+        t.string country, :null => false
       t.timestamps
     end
   end
---
+++ db/migrate/20120723175338_create_addresses.rb
@@ -0,0 +1,17 @@
+class CreateAddresses < ActiveRecord::Migration
+  def change
+    create_table :addresses do |t|
+      
+      t.integer user_id, :null => false
+      
+      t.string firstname, :null => false
+      t.string lastname, :null => false
+      t.string address, :null => false
+      t.string location, :null => false
+      t.integer postalcode, :null => false
+      t.string country, :null => false
+      
+      t.timestamps
+    end
+  end
+end
