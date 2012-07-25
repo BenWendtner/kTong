1817395e08ce1d903c4986ee87a698d4854030d1
Sebastian Poell <sebastian.poell@gmail.com>
2012-07-25 11:24:47 +0200

    some improvements admin panel not finished yet

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,10 +1,17 @@
+#encoding: utf-8
+
 ActiveAdmin.register Product do
   index do
-    column 'Kategorie', :category do |product|
+    column 'Kategorie und Name', :category do |product|
      product.category.parent.parent.name + ' / ' + product.category.parent.name + ' / ' + product.name
     end
     column 'Farbe', :color
     column 'Preis', :price
     default_actions
   end
+  
+  filter :category, :label => 'Hauptkategorie', :collection => proc { Category.where('parent_id IS NULL') }
+  filter :category, :label => 'Unterkategorie', :collection => proc { Category.where('parent_id IS NOT NULL') }
+  filter :name, :label => 'Produktname'
+  filter :color, :as => :select, :label => 'Farbe', :collection => ['Naturweiß','Möbelbraun']
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -2,4 +2,6 @@ ActiveAdmin.register User do
   index do
     column 'Email-Adresse', :email
   end
+  
+  filter :email, :label => 'Email Adresse'
 end
