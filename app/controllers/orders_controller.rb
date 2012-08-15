93a94e91c57c8c55fd58a65b979d5b45849fa864
Sebastian Poell <sebastian.poell@gmail.com>
2012-08-15 19:46:37 +0200

    bugfix

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,18 +13,16 @@ class OrdersController < ApplicationController
 
   def create
     @order = Order.new(user_id: current_user.id)
+    @order.state = 'submitted'
     @order.save
     
     current_user.cart_items.each do |cart_item|
-      order_item = OrderItem.new()
-
-        order_item.order_id = @order.id
-        
-        order_item.name = cart_item.product.name
-        order_item.color = cart_item.product.color
-        order_item.description = cart_item.product.description
-         
-        order_item.amount = cart_item.amount       
+      order_item = OrderItem.new
+      order_item.order_id = @order.id
+      order_item.name = cart_item.product.name
+      order_item.color = cart_item.product.color
+      order_item.description = cart_item.product.description
+      order_item.amount = cart_item.amount        
       
       cart_item.delete
       order_item.save
