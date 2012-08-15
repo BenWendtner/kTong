daa8a4bdf74fa7ba6d8e7d47ffe77fc4ff70e953
Sebastian Poell <sebastian.poell@gmail.com>
2012-08-15 18:56:00 +0200

    addresses

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,9 @@
 class Address < ActiveRecord::Base
-   belongs_to :user
+   
+  belongs_to :user
+   
+  validates :firstname, :lastname, :address, :location, :length => {:minimum => 1, :maximum => 100}
+  validates :postalcode, :numericality => { :only_integer => true}, :length => { :minimum => 4, :maximum => 5}
+  validates :country, :length => {:is => 2}
+  
 end
